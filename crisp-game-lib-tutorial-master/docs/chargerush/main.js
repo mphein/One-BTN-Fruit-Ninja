3e44fadabbabfc660b327b870c70c899d5a52f83
Michael Hein <mphein@ucsc.edu>
2023-11-05 14:52:58 -0800

    Added Ninja typedef

--- crisp-game-lib-tutorial-master/docs/chargerush/main.js
+++ crisp-game-lib-tutorial-master/docs/chargerush/main.js
@@ -3,12 +3,49 @@ title = "";
 description = `
 `;
 
-characters = [];
+const G = {
+	WIDTH: 100,
+	HEIGHT: 150,
+}
 
-options = {};
+characters = [
+`
+  ll
+  ll
+ccllcc 
+ccllcc 
+ccllcc 
+cc  cc 
+`
+];
+
+// JSDoc comments for typing
+/**
+ * 
+ * @typedef {{
+* pos: Vector,
+* speed: number,
+* combo: number,
+* }} Ninja
+*/
+
+/**
+ * @type { Ninja }
+ */
+let player;
+
+options = {
+	viewSize: {x: G.WIDTH, y: G.HEIGHT},
+};
 
 function update() {
 	if (!ticks) {
-		
+		player = {
+            pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
+			speed: 0,
+			combo: 0
+		}
 	}
+
+	
 }
