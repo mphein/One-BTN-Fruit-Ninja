0b45e2e4c124f13cefd5ab071e2461a92bf82af0
Michael <mphein@ucsc.edu>
2023-11-07 13:31:30 -0800

    Added some character sprites

--- crisp-game-lib-tutorial-master/docs/chargerush/main.js
+++ crisp-game-lib-tutorial-master/docs/chargerush/main.js
@@ -1,30 +1,37 @@
-title = "";
+title = "ONE BTN FRUIT NINJA";
 
 description = `
 `;
 
 const G = {
-	WIDTH: 100,
+	WIDTH: 300,
 	HEIGHT: 150,
 }
 
 characters = [
 `
-  ll
-  ll
-ccllcc 
-ccllcc 
-ccllcc 
-cc  cc 
+rlllll
+ rrrrr
+rlllll
+ lllll
+ lllll
+ l   l
+`,
+`
+  l  
+ rlr 
+rrrrr
+rrrrr
+rrrrr
+ rrr
 `
 ];
 
 // JSDoc comments for typing
 /**
- * 
  * @typedef {{
 * pos: Vector,
-* speed: number,
+* isSwinging: boolean
 * combo: number,
 * }} Ninja
 */
@@ -34,6 +41,19 @@ cc  cc
  */
 let player;
 
+
+/**
+ * @typedef {{
+ * pos: Vector,
+ * speed: number
+ * }} Fruit
+ */
+
+/**
+ * @type {Fruit []}
+ */
+let fruits
+
 options = {
 	viewSize: {x: G.WIDTH, y: G.HEIGHT},
 };
@@ -41,11 +61,39 @@ options = {
 function update() {
 	if (!ticks) {
 		player = {
-            pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
-			speed: 0,
+      pos: vec(G.WIDTH * .1, G.HEIGHT * 0.75),
+			isSwinging: false,
 			combo: 0
 		}
+
+		fruits = times(5, () => {
+			// Random number generator function
+			// rnd( min, max )
+			const posX = rnd(0, G.WIDTH);
+			const posY = rnd(0, G.HEIGHT);
+			// An object of type Star with appropriate properties
+			return {
+				// Creates a Vector
+					pos: vec(posX, posY),
+					// More RNG
+					speed: 0
+			};
+	});
+
 	}
+
+
+	color("black")
+	char("a", player.pos);
+	fruits.forEach((f) => {
+		f.pos.y += f.speed
+		f.pos.x += f.speed
+		char("b", f.pos)
+	})
+
+	box(0, G.HEIGHT * 0.75, G.WIDTH, G.HEIGHT * .25)
+	
+
 
 	
 }
