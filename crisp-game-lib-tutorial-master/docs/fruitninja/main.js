8fc544df540c6b40f4c5c46964f61ae1f40ecebb
Michael Hein <mphein@ucsc.edu>
2023-11-11 15:41:34 -0800

    Started working on the bar timing system

--- crisp-game-lib-tutorial-master/docs/fruitninja/main.js
+++ crisp-game-lib-tutorial-master/docs/fruitninja/main.js
@@ -1,12 +1,20 @@
 title = "ONE BTN FRUIT NINJA";
 
+
 description = `
 `;
 
 const G = {
-	WIDTH: 300,
-	HEIGHT: 150,
+	WIDTH: 150,
+	HEIGHT: 75,
 }
+
+const BAR = {
+	WIDTH: G.WIDTH,
+	HEIGHT: 8,
+}
+
+const spawnPT = G.HEIGHT - 6
 
 characters = [
 `
@@ -54,44 +62,75 @@ let player;
  */
 let fruits
 
+/**
+ * @typedef {{
+* pos: Vector,
+* width: number,
+* height: number
+* }} Indicator
+*/
+let indicatorBar
+let hotZone
+let safeZone
+
+/**
+ * @typedef {{
+* pos: Vector,
+* speed: number
+* width: number,
+* height: number
+* }} Scroller
+*/
+let scroller
+
 options = {
 	viewSize: {x: G.WIDTH, y: G.HEIGHT},
+	theme: "pixel"
 };
 
 function update() {
 	if (!ticks) {
 		player = {
-      pos: vec(G.WIDTH * .1, G.HEIGHT * 0.75),
+      		pos: vec(5, spawnPT),
 			isSwinging: false,
 			combo: 0
 		}
 
-		fruits = times(5, () => {
+		fruits = times(1, () => {
 			// Random number generator function
 			// rnd( min, max )
-			const posX = rnd(0, G.WIDTH);
-			const posY = rnd(0, G.HEIGHT);
+			const posX = G.WIDTH
+			const posY = spawnPT
 			// An object of type Star with appropriate properties
 			return {
 				// Creates a Vector
 					pos: vec(posX, posY),
 					// More RNG
-					speed: 0
+					speed: 1
 			};
-	});
+		});
 
+		indicatorBar = {
+			pos: vec(G.WIDTH / 2, G.HEIGHT / 4),
+			width: G.WIDTH,
+			height: 8
+		}
+		console.log(indicatorBar.pos)
 	}
-
 
 	color("black")
 	char("a", player.pos);
 	fruits.forEach((f) => {
-		f.pos.y += f.speed
-		f.pos.x += f.speed
+		f.pos.x -= f.speed
 		char("b", f.pos)
+		f.pos.wrap(0, G.WIDTH, spawnPT, 0)
 	})
+	color("yellow")
+	box(0, G.HEIGHT, G.WIDTH * 2, 5)
 
-	box(0, G.HEIGHT * 0.75, G.WIDTH, G.HEIGHT * .25)
+	console.log(indicatorBar.pos.x)
+	color("red")
+	box(indicatorBar.pos.x, indicatorBar.pos.y, indicatorBar.width, indicatorBar.height)
 	
 
 
